55077275351ba8c1e3dc35b5f856748afe8a2159
Inacio Cerqueira <inaciogcerqueira@gmail.com>
2020-08-12 19:33:57 -0300

    Ajusta indentação

--- index.js
+++ index.js
@@ -6,63 +6,67 @@ const root = process.env.GITHUB_WORKSPACE || process.cwd()
 console.log('root: ', root)
 
 function fromDir(startPath, filter, callback) {
-    if (!fs.existsSync(startPath)) {
-        console.log("no dir ", startPath);
-        return;
-    }
-    var files = fs.readdirSync(startPath);
-    for (var i = 0; i < files.length; i++) {
-        var filename = path.join(startPath, files[i]);
-        if (filename.indexOf("node_modules") === -1) {
-            var stat = fs.lstatSync(filename);
-            if (stat.isDirectory()) {
-                fromDir(filename, filter, callback);
-            }
-            else if (filename.indexOf(filter) >= 0) {
-                return callback(filename);
-            };
-        };
+  if (!fs.existsSync(startPath)) {
+    console.log("no dir ", startPath);
+    return;
+  }
+  var files = fs.readdirSync(startPath);
+  for (var i = 0; i < files.length; i++) {
+    var filename = path.join(startPath, files[i]);
+    if (filename.indexOf("node_modules") === -1) {
+      var stat = fs.lstatSync(filename);
+      if (stat.isDirectory()) {
+        fromDir(filename, filter, callback);
+      }
+      else if (filename.indexOf(filter) >= 0) {
+        return callback(filename);
+      };
     };
+  };
 };
 
 var callback_npm = (file) => {
-    console.log('-- found: ', file);
-    var r = spawnSync(
-        'npm',
-        ['ci'],
-        { cwd: path.dirname(file) }
-    );
-    if (r.error) {
-        console.log(`error: ${r.error.message}`);
-    }
-    if (r.stderr) {
-        console.log(`stderr: ${r.stderr}`);
-    }
-    console.log(`stdout: ${r.stdout}`);
-    console.log(`status: ${r.status}`)
-    return r.status;
+  console.log('-- found: ', file);
+  var r = spawnSync(
+    'npm',
+    ['ci'],
+    { cwd: path.dirname(file) }
+  );
+  if (r.error) {
+    console.log(`error: ${r.error.message}`);
+  }
+  if (r.stderr) {
+    console.log(`stderr: ${r.stderr}`);
+  }
+  console.log(`stdout: ${r.stdout}`);
+  console.log(`status: ${r.status}`);
+  return r.status;
 }
 
 var callback_eslint = (file) => {
-    console.log('-- found: ', file);
-    var r = spawnSync(
-        `npx`,
-        ['eslint', '--no-inline-config', '-c', path.basename(file), '.'],
-        { cwd: path.dirname(file) }
-    );
-    if (r.error) {
-        console.log(`error: ${r.error.message}`);
-    }
-    if (r.stderr) {
-        console.log(`stderr: ${r.stderr}`);
-    }
-    console.log(`stdout: ${r.stdout}`);
-    console.log(`status: ${r.status}`)
-    return r.status;
+  console.log('-- found: ', file);
+  var r = spawnSync(
+    `npx`,
+    ['eslint', '--no-inline-config', '-c', path.basename(file), '.'],
+    { cwd: path.dirname(file) }
+  );
+  if (r.error) {
+    console.log(`error: ${r.error.message}`);
+  }
+  if (r.stderr) {
+    console.log(`stderr: ${r.stderr}`);
+  }
+  console.log(`stdout: ${r.stdout}`);
+  console.log(`status: ${r.status}`);
+  return r.status;
 }
 
-let status = 0
-status += fromDir(root, 'package.json', callback_npm)
-status += fromDir(root, '.eslintrc.json', callback_eslint)
-console.log(`exit code: ${status}`)
-process.exit(status);
+const run = () => {
+  let status = 0
+  status += fromDir(root, 'package.json', callback_npm);
+  status += fromDir(root, '.eslintrc.json', callback_eslint);
+  console.log(`exit code: ${status}`);
+  process.exit(status);
+}
+
+run();
