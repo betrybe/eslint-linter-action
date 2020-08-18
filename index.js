053df0911e5e2f8ac4417c336abbe2fdf7b24e46
Inacio Cerqueira <inaciogcerqueira@gmail.com>
2020-08-18 15:47:28 -0300

    Posta a mensagem de feedback, refatorações

--- index.js
+++ index.js
@@ -1,68 +1,106 @@
 const path = require('path');
 const fs = require('fs');
-const { spawnSync } = require("child_process");
-const root = process.env.GITHUB_WORKSPACE || process.cwd()
+const { spawnSync } = require('child_process');
+const core = require('@actions/core');
+const github = require('@actions/github');
+const buildFeedbackMessage = require('./feedbackMessage');
 
-console.log('root: ', root)
+const root = process.env.GITHUB_WORKSPACE || process.cwd();
+let eslintOutcomes = [];
 
 function fromDir(startPath, filter, callback) {
-    if (!fs.existsSync(startPath)) {
-        console.log("no dir ", startPath);
-        return;
-    }
-    var files = fs.readdirSync(startPath);
-    for (var i = 0; i < files.length; i++) {
-        var filename = path.join(startPath, files[i]);
-        if (filename.indexOf("node_modules") === -1) {
-            var stat = fs.lstatSync(filename);
-            if (stat.isDirectory()) {
-                fromDir(filename, filter, callback);
-            }
-            else if (filename.indexOf(filter) >= 0) {
-                return callback(filename);
-            };
-        };
-    };
+  let executionStatus = 0;
+
+  if (!fs.existsSync(startPath)) {
+    console.log('Path does not exist:', startPath);
+    return 1;
+  }
+
+  const files = fs.readdirSync(startPath);
+
+  for (let i = 0; i < files.length; i += 1) {
+    const filename = path.join(startPath, files[i]);
+
+    if (filename.indexOf('node_modules') !== -1) continue;
+
+    const stat = fs.lstatSync(filename);
+
+    if (stat.isDirectory()) executionStatus += fromDir(filename, filter, callback);
+    else if (filename.indexOf(filter) >= 0) executionStatus += callback(filename);
+  }
+
+  return executionStatus;
+}
+
+const logProcessConclusion = ({ error, status, stderr = '', stdout = '' }) => {
+  const parsedStderr = stderr.toString();
+  const parsedStdout = stdout.toString();
+
+  if (error) console.log('error:', error.message);
+  if (parsedStderr) console.log('stderr:', parsedStderr);
+
+  console.log('stdout:', parsedStdout);
+  console.log('status:', status);
 };
 
-var callback_npm = (file) => {
-    console.log('-- found: ', file);
-    var r = spawnSync(
-        'npm',
-        ['ci'],
-        { cwd: path.dirname(file) }
-    );
-    if (r.error) {
-        console.log(`error: ${r.error.message}`);
-    }
-    if (r.stderr) {
-        console.log(`stderr: ${r.stderr}`);
-    }
-    console.log(`stdout: ${r.stdout}`);
-    console.log(`status: ${r.status}`)
-    return r.status;
-}
+const runNpm = (file) => {
+  console.log('-- found:', file);
 
-var callback_eslint = (file) => {
-    console.log('-- found: ', file);
-    var r = spawnSync(
-        `npx`,
-        ['eslint', '--no-inline-config', '-c', path.basename(file), '.'],
-        { cwd: path.dirname(file) }
-    );
-    if (r.error) {
-        console.log(`error: ${r.error.message}`);
-    }
-    if (r.stderr) {
-        console.log(`stderr: ${r.stderr}`);
-    }
-    console.log(`stdout: ${r.stdout}`);
-    console.log(`status: ${r.status}`)
-    return r.status;
-}
+  const npmProcess = spawnSync(
+    'npm',
+    ['ci'],
+    { cwd: path.dirname(file) },
+  );
 
-let status = 0
-status += fromDir(root, 'package.json', callback_npm)
-status += fromDir(root, '.eslintrc.json', callback_eslint)
-console.log(`exit code: ${status}`)
-process.exit(status);
+  logProcessConclusion(npmProcess);
+
+  return npmProcess.status;
+};
+
+const runEslint = (file) => {
+  console.log('-- found:', file);
+
+  const eslintProcess = spawnSync(
+    'npx',
+    ['eslint', '-f', 'json', '--no-inline-config', '--no-error-on-unmatched-pattern', '-c', path.basename(file), '.'],
+    { cwd: path.dirname(file) },
+  );
+
+  logProcessConclusion(eslintProcess);
+
+  eslintOutcomes = eslintOutcomes.concat(JSON.parse(eslintProcess.stdout));
+
+  return eslintProcess.status;
+};
+
+const run = async () => {
+  try {
+    const token = core.getInput('token', { required: true });
+    const client = github.getOctokit(token);
+    const { owner, repo, number } = github.context.issue;
+    let status = 0;
+
+    status += fromDir(root, 'package.json', runNpm);
+    status += fromDir(root, '.eslintrc.json', runEslint);
+
+    console.log('Exit code:', status);
+    console.log('All errors:', eslintOutcomes);
+
+    const feedbackMessage = buildFeedbackMessage(eslintOutcomes, root);
+
+    console.log('Feedback message:\n', feedbackMessage);
+
+    await client.issues.createComment({
+      owner,
+      repo,
+      issue_number: number,
+      body: feedbackMessage,
+    });
+
+    process.exit(status);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+};
+
+run();
