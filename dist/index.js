b4fec8ecad33780bba8fd42e6499ddbee33817e9
Inacio Cerqueira <inaciogcerqueira@gmail.com>
2020-08-18 11:36:37 -0300

    Usa guard clause

--- dist/index.js
+++ dist/index.js
@@ -220,26 +220,25 @@ const buildFeedbackMessage = __webpack_require__(51);
 const root = process.env.GITHUB_WORKSPACE || process.cwd();
 let eslintOutcomes = [];
 
-core.info('root: ', root);
-
 function fromDir(startPath, filter, callback) {
   let executionStatus = 0;
+
   if (!fs.existsSync(startPath)) {
     console.log('no dir ', startPath);
     return;
   }
+
   const files = fs.readdirSync(startPath);
+
   for (let i = 0; i < files.length; i++) {
     const filename = path.join(startPath, files[i]);
-    if (filename.indexOf('node_modules') === -1) {
-      const stat = fs.lstatSync(filename);
-      if (stat.isDirectory()) {
-        executionStatus += fromDir(filename, filter, callback);
-      }
-      else if (filename.indexOf(filter) >= 0) {
-        executionStatus += callback(filename);
-      };
-    };
+
+    if (filename.indexOf('node_modules') !== -1) continue;
+
+    const stat = fs.lstatSync(filename);
+
+    if (stat.isDirectory()) executionStatus += fromDir(filename, filter, callback);
+    else if (filename.indexOf(filter) >= 0) executionStatus += callback(filename);
   };
 
   return executionStatus;
@@ -247,38 +246,47 @@ function fromDir(startPath, filter, callback) {
 
 const runNpm = (file) => {
   console.log('-- found: ', file);
+
   const npmProcess = spawnSync(
     'npm',
     ['ci'],
     { cwd: path.dirname(file) }
   );
+
   if (npmProcess.error) {
-    console.log(`error: ${npmProcess.error.message}`);
+    console.log(`npm error: ${npmProcess.error.message}`);
   }
   if (npmProcess.stderr) {
-    console.log(`stderr: ${npmProcess.stderr}`);
+    console.log(`npm stderr: ${npmProcess.stderr}`);
   }
-  console.log(`stdout: ${npmProcess.stdout}`);
-  console.log(`status: ${npmProcess.status}`);
+
+  console.log(`npm stdout: ${npmProcess.stdout}`);
+  console.log(`npm status: ${npmProcess.status}`);
+
   return npmProcess.status;
 }
 
 const runEslint = (file) => {
   console.log('-- found: ', file);
+
   const eslintProcess = spawnSync(
     'npx',
     ['eslint', '-f', 'json' ,'--no-inline-config', '--no-error-on-unmatched-pattern', '-c', path.basename(file), '.'],
     { cwd: path.dirname(file) }
   );
+
   if (eslintProcess.error) {
     console.log(`eslint error: ${eslintProcess.error.message}`);
   }
   if (eslintProcess.stderr) {
     console.log(`eslint stderr: ${eslintProcess.stderr}`);
   }
+
   eslintOutcomes = eslintOutcomes.concat(JSON.parse(eslintProcess.stdout));
+
   console.log(`eslint stdout: ${eslintProcess.stdout}`);
   console.log(`eslint status: ${eslintProcess.status}`);
+
   return eslintProcess.status;
 }
 
@@ -293,7 +301,7 @@ const run = async () => {
     status += fromDir(root, '.eslintrc.json', runEslint);
 
     console.log('Exit code:', status);
-    core.info('All errors:', eslintOutcomes);
+    console.log('All errors:', eslintOutcomes);
 
     const feedbackMessage = buildFeedbackMessage(eslintOutcomes, root);
 
