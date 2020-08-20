ad12d05ef75d2d2d5cf1a8b704163a9f7b925149
Inacio Cerqueira <inaciogcerqueira@gmail.com>
2020-08-20 18:32:30 -0300

    Substitui uso do fromDir pelos módulos que instalam os pacotes e fazem a análise do eslint

--- index.js
+++ index.js
@@ -1,69 +1,22 @@
-const path = require('path');
-const { spawnSync } = require('child_process');
 const core = require('@actions/core');
 const github = require('@actions/github');
 const buildFeedbackMessage = require('./feedbackMessage');
-const fromDir = require('./fromDir');
-
-const root = process.env.GITHUB_WORKSPACE || process.cwd();
-let eslintOutcomes = [];
-
-const logProcessConclusion = ({ error, status, stderr = '', stdout = '' }) => {
-  const parsedStderr = stderr.toString();
-  const parsedStdout = stdout.toString();
-
-  if (error) console.log('error:', error.message);
-  if (parsedStderr) console.log('stderr:', parsedStderr);
-
-  console.log('stdout:', parsedStdout);
-  console.log('status:', status);
-};
-
-const runNpm = (file) => {
-  console.log('-- found:', file);
-
-  const npmProcess = spawnSync(
-    'npm',
-    ['ci'],
-    { cwd: path.dirname(file) },
-  );
-
-  logProcessConclusion(npmProcess);
-
-  return npmProcess.status;
-};
-
-const runEslint = (file) => {
-  console.log('-- found:', file);
-
-  const eslintProcess = spawnSync(
-    'npx',
-    ['eslint', '-f', 'json', '--no-inline-config', '--no-error-on-unmatched-pattern', '-c', path.basename(file), '.'],
-    { cwd: path.dirname(file) },
-  );
-
-  logProcessConclusion(eslintProcess);
-
-  eslintOutcomes = eslintOutcomes.concat(JSON.parse(eslintProcess.stdout));
-
-  return eslintProcess.status;
-};
+const runEslint = require('./runEslint');
+const runNpm = require('./runNpm');
 
 const run = async () => {
   try {
+    const root = process.env.GITHUB_WORKSPACE || process.cwd();
     const token = core.getInput('token', { required: true });
     const client = github.getOctokit(token);
     const { owner, repo, number } = github.context.issue;
-    let status = 0;
-
-    status += fromDir(root, 'package.json', runNpm);
-    status += fromDir(root, '.eslintrc.json', runEslint);
+    const npmStatus = runNpm(root);
+    const { status: eslintStatus, outcomes: eslintOutcomes } = runEslint(root);
+    const status = npmStatus + eslintStatus;
+    const feedbackMessage = buildFeedbackMessage(eslintOutcomes, root);
 
     console.log('Exit code:', status);
     console.log('All errors:', eslintOutcomes);
-
-    const feedbackMessage = buildFeedbackMessage(eslintOutcomes, root);
-
     console.log('Feedback message:\n', feedbackMessage);
 
     await client.issues.createComment({
